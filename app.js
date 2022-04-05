8912ebd546681b566b566602161945f1c976dc47
fran04 <franjmr16@gmail.com>
2022-04-05 15:08:37 -0500

    Init: init project

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const {PrismaClient} = require('@prisma/client')
+
+const prisma = new PrismaClient()
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello Word')
+})
+
+app.listen(
+    3000,
+    ()=> console.log(`Server ready at: http://localhost:3000`)
+)
